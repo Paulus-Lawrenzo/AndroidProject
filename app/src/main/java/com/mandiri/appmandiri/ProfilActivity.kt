ab6f619773be20fbb4e14004811985b8c4f76287
Paulus Lawrenzo <pauluslawrenzo@Pauluss-MacBook-Pro.local>
2024-01-02 19:12:10 +0700

    Layouting and Card View

---
+++ app/src/main/java/com/mandiri/appmandiri/ProfilActivity.kt
@@ -0,0 +1,29 @@
+package com.mandiri.appmandiri
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mandiri.appmandiri.RegisterActivity.Companion.KEY_ALAMAT_VALUE
+import com.mandiri.appmandiri.RegisterActivity.Companion.KEY_GENDER_VALUE
+import com.mandiri.appmandiri.RegisterActivity.Companion.KEY_NAME_VALUE
+import com.mandiri.appmandiri.RegisterActivity.Companion.KEY_UMUR_VALUE
+import com.mandiri.appmandiri.databinding.ActivityProfilBinding
+
+class ProfilActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityProfilBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityProfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra(KEY_NAME_VALUE)
+        binding.tvNameProfil.text = name
+        binding.tvTitleProfil.text = name
+        val umur = intent.getStringExtra(KEY_UMUR_VALUE)
+        binding.tvUmur.text = umur
+        val alamat = intent.getStringExtra(KEY_ALAMAT_VALUE)
+        binding.tvAlamat.text = alamat
+        val gender = intent.getStringExtra(KEY_GENDER_VALUE)
+        binding.tvGender.text = gender
+    }
+}
